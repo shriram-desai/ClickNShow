0ab459524b6398e1d32aaf047ffd9275528b7439
Shree <desaishriram55@gmail.com>
2025-07-09 19:45:13 +0530

    Add HeroSection, FeaturedSection, BlurCircle, and MovieCard

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
       
       <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=> setIsOpen(!isOpen)}/>
 
-      <Link onClick={()=> {scrollTo(0,0) , setIsOpen(!isOpen)}} to='/'>Home</Link>
-      <Link onClick={()=> {scrollTo(0,0) , setIsOpen(!isOpen)}} to='/movies'>Movies</Link>
-      <Link onClick={()=> {scrollTo(0,0) , setIsOpen(!isOpen)}} to='/'>Theaters</Link>
-      <Link onClick={()=> {scrollTo(0,0) , setIsOpen(!isOpen)}} to='/'>Releases</Link>
-      <Link onClick={()=> {scrollTo(0,0) , setIsOpen(!isOpen)}} to='/favorite'>Favorites</Link>
+      <Link onClick={()=> {scrollTo(0,0) ; setIsOpen(!isOpen)}} to='/'>Home</Link>
+      <Link onClick={()=> {scrollTo(0,0) ; setIsOpen(!isOpen)}} to='/movies'>Movies</Link>
+      <Link onClick={()=> {scrollTo(0,0) ; setIsOpen(!isOpen)}} to='/'>Theaters</Link>
+      <Link onClick={()=> {scrollTo(0,0) ; setIsOpen(!isOpen)}} to='/'>Releases</Link>
+      <Link onClick={()=> {scrollTo(0,0) ; setIsOpen(!isOpen)}} to='/favorite'>Favorites</Link>
     </div>
 
     <div className='flex items-center gap-4'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import MovieCard from '../components/MovieCard'
 
 const Home = () => {
   return (
-    <div></div>
+    <>
+      <HeroSection />
+      <FeaturedSection />
+    </>
   )
 }
 
