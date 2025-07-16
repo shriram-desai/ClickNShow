7f4b955cd05af549ea6659404528d067be58dc73
Shree <desaishriram55@gmail.com>
2025-07-16 11:46:47 +0530

    Add admin panel components and routing

---
+++ client/src/components/admin/AdminNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+
+const AdminNavbar = () => {
+  return (
+    <div>
+      <Link to="/">
+        <h1>Navbar</h1>
+      </Link>
+    </div>
+  )
+}
+
+export default AdminNavbar
